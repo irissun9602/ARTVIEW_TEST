cd5468b8b134bf0d3908bcef4811739a1f9cc841
irissun9602 <iris9602@naver.com>
2018-06-10 20:47:10 +0900

    오류 import 수정

--- jwtCustom3_Controller/src/main/java/net/skhu/controller/AuthenticationController.java
+++ jwtCustom3_Controller/src/main/java/net/skhu/controller/AuthenticationController.java
@@ -1,5 +1,86 @@
 package net.skhu.controller;
 
+import net.skhu.security.JwtTokenUtil;
+
+import net.skhu.model.AuthToken;
+
+import net.skhu.model.LoginUser;
+
+import net.skhu.model.User;
+
+import net.skhu.model.UserMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.security.authentication.AuthenticationManager;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import org.springframework.security.core.Authentication;
+
+import org.springframework.security.core.AuthenticationException;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import org.springframework.web.bind.annotation.*;
+
+
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+
+@RestController
+
+@RequestMapping("/token")
+
 public class AuthenticationController {
+
+
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+
+
+    @Autowired
+    private UserMapper userMapper;
+
+
+
+    @RequestMapping(value = "/generate-token", method = RequestMethod.POST)
+
+    public ResponseEntity<?> register(@RequestBody LoginUser loginUser) throws AuthenticationException {
+
+
+
+        final Authentication authentication = authenticationManager.authenticate(
+
+                new UsernamePasswordAuthenticationToken(
+
+                        loginUser.getLogin_id(),
+
+                        loginUser.getLogin_id()
+
+                )
+
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        final User user = userMapper.findByLogin_id(loginUser.getLogin_id());
+
+        final String token = jwtTokenUtil.generateToken(user);
+
+        return ResponseEntity.ok(new AuthToken(token));
+
+    }
+
+
 
 }
--- jwtCustom3_Controller/src/main/java/net/skhu/security/WebSecurityConfig.java
+++ jwtCustom3_Controller/src/main/java/net/skhu/security/WebSecurityConfig.java
@@ -1,130 +1,44 @@
 package net.skhu.security;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.context.annotation.Bean;
-
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.http.HttpMethod;
-
-import org.springframework.security.authentication.AuthenticationManager;
-
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import org.springframework.security.config.http.SessionCreationPolicy;
-
 import org.springframework.security.core.userdetails.UserDetailsService;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
-
-import javax.annotation.Resource;
-
-
-
 @Configuration
-
 @EnableWebSecurity
-
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	private UserDetailsService userDetailsService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-
-
-    @Resource(name = "userService")
-
-    private UserDetailsService userDetailsService;
-
-
-
-    @Autowired
-
-    private JwtAuthenticationEntryPoint unauthorizedHandler;
-
-
-
-    @Override
-
-    @Bean
-
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-
-        return super.authenticationManagerBean();
-
+    public WebSecurityConfig(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable().authorizeRequests()
+        .antMatchers("/").permitAll()
+        .antMatchers(HttpMethod.POST, "/login").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        // We filter the api/login requests
+        .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+                UsernamePasswordAuthenticationFilter.class)
+        // And filter other requests to check the presence of JWT in header
+        .addFilterBefore(new JwtAuthenticationFilter(),
+                UsernamePasswordAuthenticationFilter.class);
+  }
 
-
-    @Autowired
-
-    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.userDetailsService(userDetailsService)
-
-                .passwordEncoder(encoder());
-
-    }
-
-
-
-    @Bean
-
-    public JwtAuthenticationFilter authenticationTokenFilterBean() throws Exception {
-
-        return new JwtAuthenticationFilter();
-
-    }
-
-
-
-    @Override
-
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.cors().and().csrf().disable().
-
-                authorizeRequests()
-
-                .antMatchers("/token/*", "/signup").permitAll()
-
-                .anyRequest().authenticated()
-
-                .and()
-
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        http
-
-                .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
-
-    }
-
-
-
-    @Bean
-
-    public BCryptPasswordEncoder encoder(){
-
-        return new BCryptPasswordEncoder();
-
-    }
-
-
-
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    // Create a default account
+	  auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+  }
 }
