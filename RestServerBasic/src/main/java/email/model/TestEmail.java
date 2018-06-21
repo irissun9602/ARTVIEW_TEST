48cf22bf2d4c0cb2106c8d7ac8443e6de6252ab1
irissun9602 <iris9602@naver.com>
2018-06-22 05:24:50 +0900

    이메일 모델 추가

---
+++ RestServerBasic/src/main/java/email/model/TestEmail.java
@@ -0,0 +1,35 @@
+package email.model;
+
+public class TestEmail {
+	private String sender;
+	private String recipient;
+
+	private String subject;
+	private String content;
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getRecipient() {
+		return recipient;
+	}
+	public void setRecipient(String recipient) {
+		this.recipient = recipient;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	
+}
