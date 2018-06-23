1e0c677f096f7e953ae8a6c52eaf7c65ffd446c0
irissun9602 <iris9602@naver.com>
2018-06-23 18:25:28 +0900

    임시 비밀번호 발급 메소드 추가

---
+++ RestServerBasic/src/main/java/net/skhu/email/service/UserService.java
@@ -0,0 +1,33 @@
+package net.skhu.email.service;
+
+import org.springframework.stereotype.Service;
+
+import net.skhu.dto.Student;
+import net.skhu.email.model.TestEmail;
+import net.skhu.email.utils.Encryption;
+import net.skhu.email.utils.RandomPassword;
+import net.skhu.mapper.StudentMapper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+@Service
+public class UserService {
+
+	@Autowired
+	StudentMapper studentMapper;
+	
+	
+	public String setNewPassword(Student student) throws MessagingException {
+		String newPassword = RandomPassword.getRamdomPassword(4);
+		String setPassword = Encryption.encrypt(newPassword, Encryption.MD5);
+		studentMapper.updatePassword(setPassword, student.getEmail(), student.getName());
+		
+		
+		
+		
+		
+		return newPassword;
+	
+}
