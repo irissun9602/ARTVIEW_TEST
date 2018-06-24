325d64f687a406d5eba88f84e2856ae749a614a9
irissun9602 <iris9602@naver.com>
2018-06-24 17:18:28 +0900

    @RequestBody 비밀번호 설정 모델

---
+++ RestServerBasic/src/main/java/net/skhu/email/model/EmailAndName.java
@@ -0,0 +1,21 @@
+package net.skhu.email.model;
+
+public class EmailAndName {
+
+	String email;
+	String name;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
