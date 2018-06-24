2a2b578a1badf2ab73205dd8d914a1c0c2c69085
irissun9602 <iris9602@naver.com>
2018-06-24 17:55:26 +0900

    findUserByEmailAndName으로 변경 이름 조건 추가

--- RestServerBasic/src/main/java/net/skhu/mapper/UserMapper.java
+++ RestServerBasic/src/main/java/net/skhu/mapper/UserMapper.java
@@ -3,12 +3,12 @@ package net.skhu.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import net.skhu.dto.Student;
 import net.skhu.dto.User;
+import net.skhu.email.model.EmailAndName;
 
 @Mapper
 public interface UserMapper {
-	User findByEmail(String email);
+	User findByEmailAndName(EmailAndName en);
 	void updatePassword(@Param("password") String password, @Param("email")String email);
 	
 	
